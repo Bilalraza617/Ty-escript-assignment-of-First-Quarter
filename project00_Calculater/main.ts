7055acd17dbea5869d968299198e796329d28a7c
Bilalraza617 <anwarbilal617@gmail.com>
2024-05-26 20:59:29 +0500

    Calculatre

---
+++ project00_Calculater/main.ts
@@ -0,0 +1,50 @@
+// import inquirer
+
+import inquirer from "inquirer";
+
+// get answer
+
+const answer = await inquirer.prompt([
+  {
+    name: "firstNumber",
+    type: "number",
+    message: "PLease Enter First Number : ",
+  },
+  {
+    name: "secondNumber",
+    type: "number",
+    message: "Please enter Second Number : ",
+  },
+  {
+    name: "operater",
+    type: "list",
+    message: "Please enter Operater : ",
+    choices: ["+", "-", "*", "/", "%"],
+  },
+]);
+
+// Operaions
+
+let OperaionsAns;
+
+switch (answer.operater) {
+  case "+":
+    OperaionsAns = answer.firstNumber + answer.secondNumber;
+    break;
+  case "-":
+    OperaionsAns = answer.firstNumber - answer.secondNumber;
+    break;
+  case "*":
+    OperaionsAns = answer.firstNumber * answer.secondNumber;
+    break;
+  case "/":
+    OperaionsAns = answer.firstNumber / answer.secondNumber;
+    break;
+  case "%":
+    OperaionsAns = answer.firstNumber % answer.secondNumber;
+    break;
+}
+
+// Show Operation Ans
+
+console.log(OperaionsAns);
