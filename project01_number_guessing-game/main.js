a870285a7f4ebf372a353b44383a7cec1cad1bf3
Bilalraza617 <anwarbilal617@gmail.com>
2024-05-28 05:57:34 +0500

    update for upload on npm

--- project01_number_guessing-game/main.js
+++ project01_number_guessing-game/main.js
@@ -1,29 +1,31 @@
+#! usr/bin/env node
+
 import inquirer from "inquirer";
 //Get Name
 const userNme = await inquirer.prompt([
-    { name: "name", message: "Plaese Enter Your Name : " },
+  { name: "name", message: "Plaese Enter Your Name : " },
 ]);
 console.log(`Welcome ${userNme.name} let's start : `);
 // Generate Random Number
 let randomNumber = Math.floor(Math.random() * 10);
 //Get Number
 let userNumber = await inquirer.prompt([
-    {
-        name: "number",
-        message: "Choose any Number between 1 to 10 : ",
-    },
+  {
+    name: "number",
+    message: "Choose any Number between 1 to 10 : ",
+  },
 ]);
 //Display Correct Number
 console.log("Correct Number is : " + randomNumber);
 // Loop For repeat Game
 while (randomNumber !== Number(userNumber.number)) {
-    userNumber = await inquirer.prompt([
-        {
-            name: "number",
-            message: "Choose any Number between 1 to 10 : ",
-        },
-    ]);
-    randomNumber = Math.floor(Math.random() * 10);
-    console.log("Correct Number is : " + randomNumber);
+  userNumber = await inquirer.prompt([
+    {
+      name: "number",
+      message: "Choose any Number between 1 to 10 : ",
+    },
+  ]);
+  randomNumber = Math.floor(Math.random() * 10);
+  console.log("Correct Number is : " + randomNumber);
 }
 console.log(`Mubarak ho, You are winner ${userNme.name}`);
